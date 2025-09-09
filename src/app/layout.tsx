86f303fb91fb25cc52565e7d87970b5bfed147b6
Washington Ribeiro <washinuchiha@gmail.com>
2025-09-09 05:31:40 -0300

    feat: applying global and individual metadata to pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,24 @@
 import { Header } from "@/components/header";
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Aula NextJS do Zero!",
+  description: "Aprendendo Next JS do zero com Sujeito Programador!",
+  openGraph: {
+    title: "Aprendendo Next JS!",
+    description: "Aprendendo Next JS do zero com Sujeito Programador!",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+};
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home - Aula Next JS do zero!",
+  description: "Aprendendo Next JS do zero com Sujeito Programador!",
+  openGraph: {
+    title: "Home - Aprendendo Next JS!",
+    description: "Aprendendo Next JS do zero com Sujeito Programador!",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+};
+
 export default function Home() {
-  return(
+  return (
     <div>
       <h1>Home</h1>
     </div>
-  )
+  );
 }
